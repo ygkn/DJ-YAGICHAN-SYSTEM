3583dbdf2687b9c677d9f11d0eb2078f2cf97626
Yugo Yagita <2000ygkn0713@gmail.com>
2017-04-09 09:37:18 +0900

    Fix module requirements

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
-const app  = require("express")();
-const http = require("http").Server(app);
-const PORT = process.env.PORT || 3000;
-const path = require("path");
+const express = require("express");
+const http    = require("http").Server(app);
+const PORT    = process.env.PORT || 3000;
+const path    = require("path");
+const app     = express();
 
 app.get("/", (req, res) =>
   res.sendFile(path.join(__dirname, "index.html")));
