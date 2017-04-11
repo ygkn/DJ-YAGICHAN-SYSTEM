6abc2a2c1e4bf615f821de93061c5c7cfcb22ed4
Yugo Yagita <2000ygkn0713@gmail.com>
2017-04-11 10:21:16 +0000

    Connect Socket.IO

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app     = express();
 const http    = require("http").Server(app);
 const PORT    = process.env.PORT || 3000;
 const path    = require("path");
+const io      = require('socket.io')(http);
 
 app.set('view engine', 'pug');
 app.use(express.static("public"));
@@ -10,4 +11,8 @@ app.use(express.static("public"));
 app.get('/', (req, res) => 
   res.render('index'));
 
-http.listen(PORT, () => console.log(`listening on ${PORT}`))
+http.listen(PORT, process.env.IP, () => console.log(`listening on ${PORT}`))
+
+io.on('connection', (socket) => {
+  console.log('a user connected' + socket.id);
+});
