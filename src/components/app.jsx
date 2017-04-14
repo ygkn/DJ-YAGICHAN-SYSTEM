4d41b3fa5dbe6c4fff8dfc76988f8d80cc3dc7d4
Yugo Yagita <2000ygkn0713@gmail.com>
2017-04-14 14:13:01 +0000

    use Component class

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,8 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const App = () => {
+class App extends Component {
+  constructor(){
+    super();
+    this.state = {query: "", userName: ""};
+  }
+  
+  inputQueryHandler(e) {
+    this.setState({query: e.target.value});
+  }
+  
+  inputUserNameHandler(e) {
+    this.setState({userName: e.target.value});
+  }
+  
+  render() {
     return (
-        <h1>World</h1>
-    );
+      <div>
+        <h1> DJ YAGICHAN SYSTEM </h1>
+        <form>
+          <input onInput={this.inputUserNameHandler.bind(this)} value={this.state.userName} placeholder="あなたの名前を入力…" />
+          <input onInput={this.inputQueryHandler.bind(this)} value={this.state.query} placeholder="キーワード、URLを入力…" />
+        </form>
+        {(() => this.state.userName && <p>すっごーい！あなたは{this.state.userName}のフレンズなんだね！</p>)()}
+      </div>
+    )
+  }
 }
+
 export default App;
