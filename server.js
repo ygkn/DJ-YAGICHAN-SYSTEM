27bf7429ab969de6f9527d8befea5ecfefeb78f1
Yugo Yagita <2000ygkn0713@gmail.com>
2017-04-28 00:18:58 +0900

    Define static path

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
-const app  = require('express')();
-const http = require('http').Server(app);
-const io   = require('socket.io')(http);
-const PORT = process.env.PORT || 3001;
-const IP   = process.env.IP   || "localhost";
+const express = require('express');
+const app     = express();
+const http    = require('http').Server(app);
+const io      = require('socket.io')(http);
+const path    = require('path');
+const PORT    = process.env.PORT || 3001;
+const IP      = process.env.IP   || "localhost";
+
+app.use(express.static(path.resolve(__dirname, "client", "build")));
 
 io.on('connection', (socket) => {
   console.log('a user connected');
