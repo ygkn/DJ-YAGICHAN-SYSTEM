0cec3c277c1a7529890172569f11d3f1a7156c3a
Yugo Yagita <2000ygkn0713@gmail.com>
2017-04-29 00:19:16 +0900

    Configure the Fallback URL

--- server.js
+++ server.js
@@ -5,8 +5,13 @@ const io      = require('socket.io')(http);
 const path    = require('path');
 const PORT    = process.env.PORT || 3001;
 const IP      = process.env.IP   || "localhost";
+const STATIC_DIR = path.resolve(__dirname, "client", "build");
 
-app.use(express.static(path.resolve(__dirname, "client", "build")));
+app.use(express.static(STATIC_DIR));
+
+app.get('/*', (req,res) => {
+  res.sendFile(path.join(STATIC_DIR, 'index.html'))
+})
 
 io.on('connection', (socket) => {
   console.log('a user connected');
