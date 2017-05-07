f0b4c27dcc53bf518b3e4de16d1c80e7313913dd
Yugo Yagita <2000ygkn0713@gmail.com>
2017-05-07 23:51:12 +0900

    Redux

--- client.js
+++ client.js
@@ -1,3 +1,3 @@
 const args = process.argv.length > 2 ?  process.argv.slice(2) : [ 'start' ];
 const opts = { stdio: 'inherit', cwd: 'client', shell: true };
-require('child_process').spawn('npm', args, opts);
+require('child_process').spawn('yarn', args, opts);
--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,12 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import { render } from 'react-dom'
+import { createStore } from 'redux'
+import todoApp from './reducers'
+import Root from './components/Root'
 
-ReactDOM.render(
-  <App />,
+let store = createStore(todoApp)
+
+render(
+  <Root store={store} />,
   document.getElementById('root')
-);
+)
