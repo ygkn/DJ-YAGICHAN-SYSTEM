ac96af80a75eedd1f2de9c12d57dce3813012064
Yugo Yagita <2000ygkn0713@gmail.com>
2017-05-26 23:29:10 +0900

    react-devtools

--- client/src/index.js
+++ client/src/index.js
@@ -1,15 +1,18 @@
+import { createDevTools } from 'redux-devtools'
+import LogMonitor from 'redux-devtools-log-monitor'
+import DockMonitor from 'redux-devtools-dock-monitor'
+
 import React from 'react';
 import { render } from 'react-dom';
 
-import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
 import { Provider } from 'react-redux';
 
 import createSagaMiddleware from 'redux-saga';
 
-import createHistory from 'history/createBrowserHistory';
-import { Route } from 'react-router';
-
-import { ConnectedRouter, routerReducer, routerMiddleware, push } from 'react-router-redux';
+import { Router, Route} from 'react-router'
+import { createBrowserHistory } from 'history';
+import { syncHistoryWithStore, routerReducer } from 'react-router-redux'
 
 import reducers from './reducers';
 import rootSaga from './sagas';
@@ -18,32 +21,42 @@ import Login from './components/Login.jsx'
 import Post from './components/Post'
 import Dashboard from './components/Dashboard'
 
-const history = createHistory();
-
 const sagaMiddleware = createSagaMiddleware();
+
+const DevTools = createDevTools(
+  <DockMonitor toggleVisibilityKey="ctrl-h" changePositionKey="ctrl-q">
+    <LogMonitor theme="tomorrow" preserveScrollTop={false} />
+  </DockMonitor>
+)
+
 
 const store = createStore(
   combineReducers({
     ...reducers,
-    router: routerReducer
+    routing: routerReducer
   }),
-  applyMiddleware(
-    routerMiddleware(history),
-    sagaMiddleware
+  compose(
+    applyMiddleware(sagaMiddleware),
+    DevTools.instrument()
   )
 );
+
+const history = syncHistoryWithStore(createBrowserHistory(), store);
 
 sagaMiddleware.run(rootSaga);
 
 render(
   <Provider store={store}>
-    <ConnectedRouter history={history}>
-      <div>
-        <Route exact path="/" component={Login}/>
-        <Route exact path="/:roomId" component={Post}/>
-        <Route exact path="/:roomId/dashboard" component={Dashboard}/>
-      </div>
-    </ConnectedRouter>
+    <div>
+      <Router history={history}>
+        <div>
+          <Route exact path="/" component={Login}/>
+          <Route exact path="/:roomId" component={Post}/>
+          <Route exact path="/:roomId/dashboard" component={Dashboard}/>
+        </div>
+      </Router>
+      <DevTools />
+    </div>
   </Provider>,
   document.getElementById('root')
 );
