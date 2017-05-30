784c672f4b0c9b42d5b6431a0a155a164493669d
Yugo Yagita <2000ygkn0713@gmail.com>
2017-05-30 21:22:21 +0900

    Add router saga to rootSaga

--- client/src/sagas.js
+++ client/src/sagas.js
@@ -1,2 +1,10 @@
+import { createBrowserHistory as createHistory, saga as router } from 'redux-tower';
+import routes from './routes'
+import { fork } from 'redux-saga/effects'
+
+const history = createHistory();
+
 export default function* rootSaga() {
+  yield fork(router, {history, routes});
+
 }
