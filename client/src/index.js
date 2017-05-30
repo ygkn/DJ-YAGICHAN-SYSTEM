d957c9378b7f20325998d04c276991f8069cc68d
Yugo Yagita <2000ygkn0713@gmail.com>
2017-05-30 21:22:00 +0900

    redux-tower

--- client/src/index.js
+++ client/src/index.js
@@ -10,22 +10,17 @@ import { Provider } from 'react-redux';
 
 import createSagaMiddleware from 'redux-saga';
 
-import { createBrowserHistory as createHistory, reducer as router } from 'redux-tower';
+import { reducer as router } from 'redux-tower';
+import { Router } from 'redux-tower/lib/react';
 
 import reducers from './reducers';
 import rootSaga from './sagas';
-
-import Login from './components/Login.jsx'
-import Post from './components/Post'
-import Dashboard from './components/Dashboard'
 
 const DevTools = createDevTools(
   <DockMonitor toggleVisibilityKey="ctrl-h" changePositionKey="ctrl-q">
     <LogMonitor theme="tomorrow" preserveScrollTop={false} />
   </DockMonitor>
 )
-
-const history = createHistory();
 
 const sagaMiddleware = createSagaMiddleware();
 const store = createStore(
@@ -39,20 +34,12 @@ const store = createStore(
   )
 );
 
-const routerHistory = syncHistoryWithStore(history, store, { adjustUrlOnReplay: true });
-
 sagaMiddleware.run(rootSaga);
 
 render(
   <Provider store={store}>
     <div>
-      <Router history={routerHistory}>
-        <div>
-          <Route exact path="/" component={Login}/>
-          <Route exact path="/:roomId" component={Post}/>
-          <Route exact path="/:roomId/dashboard" component={Dashboard}/>
-        </div>
-      </Router>
+      <Router />
       <DevTools />
     </div>
   </Provider>,
