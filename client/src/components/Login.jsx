569b77cb51cd844672ac5454d2f28f1826cf9692
Yugo Yagita <2000ygkn0713@gmail.com>
2017-06-01 21:10:23 +0900

    import Link component from redux-tower

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Logo from './Logo';
-import { Link } from 'react-router-dom';
+import { Link } from 'redux-tower/lib/react';
 
 class Login extends React.Component {
   constructor() {
