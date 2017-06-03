316b28ccf0dd846fe29d2b974391be2b01a4ae10
Yugo Yagita <2000ygkn0713@gmail.com>
2017-06-03 21:14:13 +0900

    Fix route setting

--- client/src/routes.js
+++ client/src/routes.js
@@ -1,10 +1,21 @@
-import Login from './components/Login.jsx'
+import React from 'react';
+
+import Login from './components/Login'
 import Post from './components/Post'
 import Dashboard from './components/Dashboard'
 
-export const routes = {
+import { createBrowserHistory as createHistory, saga as router } from 'redux-tower';
+import { fork } from 'redux-saga/effects'
+
+const history = createHistory();
+
+const routes = {
   "/": "/login",
-  "/login": Login,
-  "/:roomId/post": Post,
-  "/:roomId/dashboard": Dashboard
+  "/login": <Login />,
+  "/:roomId/post": <Post />,
+  "/:roomId/dashboard": <Dashboard />
+};
+
+export default function* routesSaga() {
+  yield fork(router, {history, routes});
 }
