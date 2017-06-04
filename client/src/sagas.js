14c3257f730264a0ba1128d182cd2e543707a715
Yugo Yagita <2000ygkn0713@gmail.com>
2017-06-04 21:14:11 +0900

    Fix rootSaga

--- client/src/sagas.js
+++ client/src/sagas.js
@@ -1,10 +1,6 @@
-import { createBrowserHistory as createHistory, saga as router } from 'redux-tower';
 import routes from './routes'
 import { fork } from 'redux-saga/effects'
 
-const history = createHistory();
-
 export default function* rootSaga() {
-  yield fork(router, {history, routes});
-
+  yield fork(routes);
 }
