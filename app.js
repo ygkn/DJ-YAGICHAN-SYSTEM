6fe660bbf6a51141e34a4248f8d847b018449740
Yugo Yagita <2000ygkn0713@gmail.com>
2017-10-09 22:54:29 +0900

    Add app.js

---
+++ app.js
@@ -0,0 +1,32 @@
+import express from 'express';
+import path from 'path';
+
+const app = express();
+const port = process.env.PORT || 8080;
+
+if (process.env.NODE_ENV !== 'production') {
+  /* eslint-disable global-require, import/no-extraneous-dependencies */
+  const webpack = require('webpack');
+  const webpackDevMiddleware = require('webpack-dev-middleware');
+  const webpackHotMiddleware = require('webpack-hot-middleware');
+  const config = require('./webpack.config.dev.babel').default;
+  const compiler = webpack(config);
+
+  app.use(webpackHotMiddleware(compiler));
+  app.use(webpackDevMiddleware(compiler, {
+    noInfo: true,
+    publicPath: config.output.publicPath,
+  }));
+}
+
+// Serve static files
+app.use(express.static(path.resolve(__dirname, 'static')));
+
+console.log(`Served: http://localhost:${port}`);
+app.listen(port, (err) => {
+  if (err) {
+    console.error(err);
+  } else {
+    console.info(`==> 🌎  Listening on port ${port}. Open up http://localhost:${port}/ in your browser.`);
+  }
+});
